9f69e43735093be192cf475f1791f01ebe9053fa
Ayaan Bargir <ayaanbargir7@gmail.com>
2025-10-18 19:14:36 +0530

    Rename parameters in clone function for clarity

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
-def call(String url, String branch){
+def call(String GitUrl, String GitBranch){
   echo "This is cloning the code!"
-  git url: url, branch: branch
+  git url: "${GitUrl}", branch: "${GitBranch}"
   echo "Clonned Repo Successful!"
 }
